ba33312643c5ee6da0453977cc594e3ab916d042
Vincent <vcheong0202@gmail.com>
2024-11-01 13:47:40 -0700

    Added fixtures for menuItems

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbMenuItemFixtures = {
+    oneMenuItem: {
+        id: 1,
+        diningCommonsCode: "ortega",
+        name: "Baked Pesto Pasta with Chicken",
+        station: "Entree Specials",
+    },
+    threeMenuItems: [
+      {
+        id: 2,
+        diningCommonsCode: "ortega",
+        name: "Tofu Banh Mi Sandwich (v)",
+        station: "Entree Special",
+      },
+      {
+        id: 3,
+        diningCommonsCode: "ortega",
+        name: "Chicken Caesar Salad",
+        station: "Entrees",
+      },
+      {
+        id: 5,
+        diningCommonsCode: "portola",
+        name: "Cream of Broccoli Soup (v)",
+        station: "Greens & Grains",
+      },
+    ],
+  };
+  
+  export { ucsbMenuItemFixtures };
+  
