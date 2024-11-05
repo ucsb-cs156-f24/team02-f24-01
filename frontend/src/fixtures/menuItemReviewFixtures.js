9cd60c1fac8d22e770749def4500813dd6afb4f0
Junhyung Yoon <junhyungyoon@169-231-118-251.wireless.ucsb.edu>
2024-11-05 15:14:20 -0800

    edit

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,36 +1,36 @@
-const menuItemReviewFixtures = {
-    oneMenuItemReview: [
-      {
-        itemId: 1,
-        reviewerEmail: "test1@ucsb.edu",
-        stars: 4,
-        dateReviewed: "2024-07-29T10:10:10",
-        comments: "test comment 1"
-      },
-    ],
+// const menuItemReviewFixtures = {
+//     oneMenuItemReview: [
+//       {
+//         itemId: 1,
+//         reviewerEmail: "test1@ucsb.edu",
+//         stars: 4,
+//         dateReviewed: "2024-07-29T10:10:10",
+//         comments: "test comment 1"
+//       },
+//     ],
   
-    threeMenuItemReviews: [
-      {
-        id: 2,
-        name: "Cristino's Bakery",
-        description:
-          "This place is takeout only.  It may look mostly like a bakery with Mexican pastries, but it also has amazing burritos and tacos",
-      },
+//     threeMenuItemReviews: [
+//       {
+//         id: 2,
+//         name: "Cristino's Bakery",
+//         description:
+//           "This place is takeout only.  It may look mostly like a bakery with Mexican pastries, but it also has amazing burritos and tacos",
+//       },
   
-      {
-        id: 3,
-        name: "Freebirds",
-        description: "Burrito joint, and iconic Isla Vista location",
-      },
+//       {
+//         id: 3,
+//         name: "Freebirds",
+//         description: "Burrito joint, and iconic Isla Vista location",
+//       },
   
-      {
-        id: 4,
-        name: "Ca' Dario Cucina Italiana",
-        description:
-          "White tablecloth Italian restaurant, with great pasta and pizza",
-      },
-    ],
-  };
+//       {
+//         id: 4,
+//         name: "Ca' Dario Cucina Italiana",
+//         description:
+//           "White tablecloth Italian restaurant, with great pasta and pizza",
+//       },
+//     ],
+//   };
   
-  export { menuItemReviewFixtures };
+//   export { menuItemReviewFixtures };
   
