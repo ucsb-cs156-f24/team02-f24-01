1f05f8043a8cf44631fdf7b501d38dd7dc0c7805
Cameron B <cameronbyrne@ucsb.edu>
2024-11-06 16:43:13 -0800

    Copied over backend CRUD for helprequest

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -0,0 +1,11 @@
+package edu.ucsb.cs156.example.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.ucsb.cs156.example.entities.HelpRequest;
+
+@Repository
+public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {
+
+}
