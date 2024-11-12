51fd79ca2816d01c209a44fbb28dbc5eed8e77fd
Cameron B <cameronbyrne@ucsb.edu>
2024-11-11 23:57:13 -0800

    Added helprequest index page, tests, stories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,9 @@ import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
 import RestaurantCreatePage from "main/pages/Restaurants/RestaurantCreatePage";
 import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
 
-import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
-import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
-import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
+import PlaceholderIndexPage from "main/pages/HelpRequest/HelpRequestIndexPage";
+import PlaceholderCreatePage from "main/pages/HelpRequest/PlaceholderCreatePage";
+import PlaceholderEditPage from "main/pages/HelpRequest/PlaceholderEditPage";
 
 import UCSBOrganizationIndexPage from "main/pages/UCSBOrganization/UCSBOrganizationIndexPage";
 import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
