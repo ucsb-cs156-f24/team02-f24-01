d68cbfc62be007f40eaea130aaf9459feb030f4b
Vincent <vcheong0202@gmail.com>
2024-11-11 19:49:10 -0800

    fixed files with prettier

--- frontend/src/tests/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemEditPage.test.js
+++ frontend/src/tests/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemEditPage.test.js
@@ -165,7 +165,7 @@ describe("UCSBDiningCommonsMenuItemEditPage tests", () => {
           name: "Seasame salad",
           station: "Veggies",
         }),
-      ); 
+      );
     });
 
     test("Changes when you click Update", async () => {
